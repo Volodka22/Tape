71f48fc40d23003f1414254e9f531d12c96d72a0
volodya339 <smagliy.vova@mail.ru>
2022-12-29 17:07:28 +0200

    delete template from sort

--- src/tapes/tape.cpp
+++ src/tapes/tape.cpp
@@ -19,4 +19,31 @@ namespace tapes {
             prev();
         }
     }
+
+    void sort(tape const &input, tape &output, tape_manager &&manager, size_t batch_size) {
+        assert(input.size() == output.size());
+        input.to_first();
+        std::vector<int32_t> array;
+        array.reserve(batch_size);
+        for (size_t start = 0; start < input.size(); start += batch_size) {
+            for (int i = 0; i < batch_size && start + i < input.size(); i++) {
+                array.push_back(input.current());
+                if (!input.is_last()) {
+                    input.next();
+                }
+            }
+            std::sort(array.begin(), array.end());
+            manager.add(array);
+            array.clear();
+        }
+
+        output.to_last();
+
+        while (!manager.empty()) {
+            output.set(manager.get_max());
+            if (!output.is_first()) {
+                output.prev();
+            }
+        }
+    }
 }
--- src/tapes/tape.h
+++ src/tapes/tape.h
@@ -83,34 +83,6 @@ namespace tapes {
      * @param manager
      * @param batch_size - size of max memory batch
      */
-    template<typename TAPE,
-            typename MANAGER,
-            typename = std::enable_if<std::is_base_of_v<tape, TAPE> && std::is_base_of_v<tape_manager, MANAGER>>>
-    void sort(TAPE const &input, TAPE &output, MANAGER manager, size_t batch_size) {
-        assert(input.size() == output.size());
-        input.to_first();
-        std::vector<int32_t> array;
-        array.reserve(batch_size);
-        for (size_t start = 0; start < input.size(); start += batch_size) {
-            for (int i = 0; i < batch_size && start + i < input.size(); i++) {
-                array.push_back(input.current());
-                if (!input.is_last()) {
-                    input.next();
-                }
-            }
-            std::sort(array.begin(), array.end());
-            manager.add(array);
-            array.clear();
-        }
-
-        output.to_last();
-
-        while (!manager.empty()) {
-            output.set(manager.get_max());
-            if (!output.is_first()) {
-                output.prev();
-            }
-        }
-    }
+    void sort(tape const &input, tape &output, tape_manager&& manager, size_t batch_size);
 
 }
